447edd2276cd9593a09e745fe7e7c22e95f1f1f5
Akzhig1t01 <akzhigit1010@gmail.com>
2025-12-15 08:04:34 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args)  {
-        System.out.println("Hello, workd! My name is Akzhigit Group SE-2523");
+        System.out.println("Hello, workd! My name is Akzhigit Group SE-2523 ");
     }
 }
